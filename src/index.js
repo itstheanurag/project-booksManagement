c92fa1e797c271838f436683ad80085754e73bf2
itstheanurag <gauravanurag36@gmail.com>
2022-05-09 16:07:20 +0530

    gitIgnore

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
 
-const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://itstheanurag:gaurav9878764239@cluster0.dirde.mongodb.net/BookManagement-Group9", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
 
 
-app.use()
-app.use()
+app.use('/', route);
 
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
